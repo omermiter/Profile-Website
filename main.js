f010745542fefa1e5b07d5edb8acd03b2a97748f
Omer Mitran <omer3107@gmail.com>
2024-07-11 23:20:17 +0300

    Added favicon and SVG's

---
+++ main.js
@@ -0,0 +1,11 @@
+const x = document.querySelector(".twitter");
+
+x.onclick = ()=>{
+    window.open("https://x.com/Omermitran", "_blank");
+};
+
+const github = document.querySelector(".github");
+
+github.onclick = () => {
+    window.open("https://github.com/omermiter", "_blank");
+}
