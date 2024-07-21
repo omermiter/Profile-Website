67d0da432f2b67067f582c9c49c0e129d5f404ca
Omer Mitran <omer3107@gmail.com>
2024-07-21 11:56:56 +0300

    changed the design

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ const github = document.querySelector(".github");
 github.onclick = () => {
     window.open("https://github.com/omermiter", "_blank");
 }
+
+
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+        const loadingScreen = document.getElementById('loading-screen');
+        loadingScreen.style.opacity = '0';
+        loadingScreen.style.visibility = 'hidden';
+    }, 2000); // Adjust the time (in milliseconds) as needed
+});
+
+
